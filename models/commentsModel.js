ac3479311a1b45b5877e2414de64f03f0eeef587
MEC43 <myung0403@gmail.com>
2024-06-25 23:56:25 +0900

    240625 chae

---
+++ models/commentsModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const CommentSchema = new Schema({
+  postId: { type: String, required: true },
+  userId: { type: String, required: true },
+  content: { type: String, required: true },
+},
+  { timestamps: true }
+);
+
+const CommentModel = mongoose.model('Comment', CommentSchema);
+module.exports = CommentModel
---
+++ models/postModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose
+
+const PostSchema = new Schema({
+  userId: { type: String, required: true, unique: true },
+  username: { type: String },
+  category: { type: String, required: true, unique: true },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  image: { type: String, default: null },
+  region: { type: String, required: true },
+  likeCount: { type: Number, default: 0 },
+  commentsCount: { type: Number, default: 0 },
+  coordiReview: { type: Boolean, default: false },
+  coordiGood: { type: Number, default: 0 },
+  coordiSoso: { type: Number, default: 0 },
+  coordiBad: { type: Number, default: 0 },
+},
+  { timestamps: true }
+)
+
+const PostModel = mongoose.model('Post', PostSchema)
+module.exports = PostModel

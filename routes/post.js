67f6fcf2dbd013c1f40be9fdc4e617cbaf22f0f8
MEC43 <myung0403@gmail.com>
2024-07-02 12:44:52 +0900

    240702 1시 Chae

--- routes/post.js
+++ routes/post.js
@@ -9,7 +9,7 @@ const jwtSecret = process.env.JWT_SECRET
 
 // 포스트 이미지 저장 경로 multer 설정
 const postImgUpload = multer.diskStorage({
-  destination: 'codiUploads/postImg/',
+  destination: 'uploads/postImg/',
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname))
   }
@@ -19,6 +19,7 @@ const postImgUp = multer({ storage: postImgUpload })
 // 몽구스 모델 호출
 const Post = require('../models/postModel')
 const Like = require('../models/likeModel');
+const CoordiReview = require('../models/coordiReview')
 
 // ---- 커뮤니티 디테일 페이지 정보 get요청
 router.get('/postDetail/:postId', async (req, res) => {
@@ -64,7 +65,6 @@ router.post('/writePost', postImgUp.single('file'), async (req, res) => {
   console.log('글쓰기', postCate, region, onReview, title, content);
   console.log('이미지 경로', path);
 
-  //회원가입 로그인 기능 구현외면 userId, username 하기
   const { token } = req.cookies;
   jwt.verify(token, jwtSecret, {}, async (err, userInfo) => {
     if (err) throw err;
@@ -96,24 +96,76 @@ router.post('/writePost', postImgUp.single('file'), async (req, res) => {
 
 // ---- 좋아요 클릭 
 router.post('/like', async (req, res) => {
-  console.log(req.body);
+  console.log('좋아요클릭', req.body);
   try {
-    const { postId, isLike } = req.body;
+    const { postId, isLike, userId } = req.body;
     console.log(isLike);
-    const updateLikeCoun = await Post.findByIdAndUpdate(postId, { $inc: { likeCount: isLike ? 1 : -1 } })
-    res.json({ success: true, likes: updateLikeCoun.likeCount })
+    const updateLikeCount = await Post.findByIdAndUpdate(postId, { $inc: { likeCount: isLike ? 1 : -1 } })
+
+    const likeDoc = await Like.findOne({ userId })
+
+    console.log(likeDoc);
+
+    if (likeDoc) {
+      const isPostIdAleady = likeDoc.postId.includes(postId);
+
+      if (isPostIdAleady) {
+        const likeListDel = await Like.findOneAndUpdate({ userId }, { $pull: { postId: postId } })
+        console.log('좋아요 취소', likeListDel);
+
+      } else {
+        const likeListUpdate = await Like.findOneAndUpdate({ userId }, { $push: { postId: postId } })
+        console.log('좋아요 추가', likeListUpdate);
+      }
+    } else {
+      const likeListCreate = await Like.create({
+        postId: [postId],
+        userId
+      })
+      console.log('좋아요 리스트 생성', likeListCreate);
+    }
+    res.json({ success: true, likes: updateLikeCount.likeCount })
 
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: '좋아요 서버 에러' });
   }
-
 })
 
-// ---- 코디 리뷰 클릭
+// ---- 사용자 좋아요 리스트 체크용 get요청
+router.get('/likeCheck/:postId', async (req, res) => {
+  // const { postId } = req.params
+  const { userId } = req.query
+  // console.log('좋아요 체크용 포스트 아이디', postId, userId);
+  try {
+    const likeList = await Like.findOne({ userId })
+    // console.log(likeList);
+    res.json(likeList)
+
+  } catch (error) {
+    console.error('좋아요 체크용 리스트 서버 에러', { error });
+    res.status(500).json('좋아요 체크용 리스트 서버 에러', { error });
+  }
+})
+
+// ---- 유저 코디리뷰 상태 체크용 get요청
+router.get('/reviewCheck/:postId', async (req, res) => {
+  const { postId } = req.params
+  const { userId } = req.query
+  console.log('유저 코디리뷰 상태 체크용', postId, userId);
+  try {
+    const reviewCheckDoc = await CoordiReview.findOne({ postId, userId })
+    console.log(reviewCheckDoc);
+    res.json(reviewCheckDoc)
+
+  } catch (error) {
+    console.error('코디리뷰 상태 체크 get요청 서버 에러', { error });
+    res.status(500).json('코디리뷰 상태 체크 get요청 서버 에러', { error });
+  }
+})
+
+// ---- 코디 리뷰 클릭(카운트 변경)
 router.put('/updateReview/:postId', async (req, res) => {
-  console.log(req.body)
-  console.log(req.params)
   const { postId } = req.params
   const { btnType, count } = req.body
 
@@ -121,13 +173,38 @@ router.put('/updateReview/:postId', async (req, res) => {
   const updateCount = count === 'increment' ? 1 : -1;
 
   try {
-    const updateReview = await Post.findByIdAndUpdate(postId, { $inc: { [updateField]: updateCount } })
-    console.log(updateReview);
+    const updateReview = await Post.findByIdAndUpdate(postId, { $inc: { [updateField]: updateCount } }, { new: true })
+    // console.log(updateReview);
     res.json(updateReview)
   } catch (error) {
     console.error('코디 리뷰 서버 에서', error);
   }
+})
 
+// ---- 코디 리뷰 클릭(유저 코디리뷰 상태 저장)
+router.put('/saveCoordiRevw', async (req, res) => {
+  const { userId, postId, btnType } = req.body
+  console.log('코디리뷰 상태저장', userId, postId, btnType);
+  try {
+    const coordiReviewDoc = await CoordiReview.findOne({ userId, postId })
+    console.log(coordiReviewDoc);
+
+    if (coordiReviewDoc) {
+      //유저의 해당 게시물에 대한 리뷰가 있는 경우 버튼타입 업데이트
+      console.log('버튼타입 업데이트');
+      const crdiRvUpdate = await CoordiReview.findOneAndUpdate({ userId, postId }, { btnType }, { new: true })
+      res.json(crdiRvUpdate)
+    } else {
+      console.log('게시물 코디리뷰 생성');
+      // 유저의 해당 게시물에 대한 리뷰가 없다면
+      const CoordiRevw = await CoordiReview.create({ userId, postId, btnType })
+      res.json(CoordiRevw)
+    }
+
+  } catch (error) {
+    console.error('유저 코디리뷰 상태 저장 서버 에러', { error });
+    res.status(500).json('유저 코디리뷰 상태 저장 서버 에러', { error });
+  }
 })
 
 // ---- 글 삭제
